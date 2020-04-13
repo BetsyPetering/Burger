d459ccfd41a0d650891bb349918a207e17ab0ab8
PinkAttitude77 <betsy.petering@sbcglobal.net>
2020-04-13 14:52:31 -0700

    Making Heroku ready

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burger (name) VALUES ('Yummy');
-INSERT INTO burger (name) VALUES ('avocado');
-INSERT INTO burger (name, devoured) VALUES ('bacon', true);
-INSERT INTO burger (name, devoured) VALUES ('veggie', true);
-INSERT INTO burger (name, devoured) VALUES ('cheese', true);
-INSERT INTO burger (name) VALUES ('in-n-out');
+INSERT INTO burgers (name) VALUES ('Yummy');
+INSERT INTO burgers (name) VALUES ('avocado');
+INSERT INTO burgers (name, devoured) VALUES ('bacon', true);
+INSERT INTO burgers (name, devoured) VALUES ('veggie', true);
+INSERT INTO burgers (name, devoured) VALUES ('cheese', true);
+INSERT INTO burgers (name) VALUES ('in-n-out');
